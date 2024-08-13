5002bdbb49735f31326c9aa289ae64c43e997ef0
LordButterHam <basvercruysse@gmail.com>
2024-08-13 15:07:05 +0200

    Trained a strong model (97% acc)

--- predict.py
+++ predict.py
@@ -4,9 +4,7 @@ file = str(input("Path to data...: "))
 model = str(input("Model...: "))
 
 file = '/home/bavercru/Documents/Visual Code - workspace/CRF/data/ghent-city-directories/wegwijzer-1913-entries-sample.txt'
-model = 'test.pkg'
 
 classifier = Classifier.Classifier()
 classifier.listen(file, model)
-# /home/bavercru/Documents/Visual Code - workspace/CRF/data/ghent-city-directories/wegwijzer-labeled-p533-543-training.csv
 
