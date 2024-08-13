c050f972d043ebd41efeb3f94cd6258ad6dbfc7a
LordButterHam <basvercruysse@gmail.com>
2024-08-13 15:08:04 +0200

    Housekeeping

--- predict.py
+++ predict.py
@@ -3,8 +3,6 @@ from cdparser import Classifier
 file = str(input("Path to data...: "))
 model = str(input("Model...: "))
 
-file = '/home/bavercru/Documents/Visual Code - workspace/CRF/data/ghent-city-directories/wegwijzer-1913-entries-sample.txt'
-
 classifier = Classifier.Classifier()
 classifier.listen(file, model)
 
